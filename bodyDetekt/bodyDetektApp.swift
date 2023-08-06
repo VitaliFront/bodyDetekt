4d36d532aa2b78a974ead23d8868579053acd74c
Vitali Klopau <vitaliklopau@gmail.com>
2023-08-07 01:11:13 +0200

    Initial Commit

---
+++ bodyDetekt/bodyDetektApp.swift
@@ -0,0 +1,17 @@
+//
+//  bodyDetektApp.swift
+//  bodyDetekt
+//
+//  Created by Vitali Klopau on 07.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct bodyDetektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
